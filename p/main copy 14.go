ddc53d33822585321892de3800590cea518108b7
Kayque lopes <kaiquelopes.601@gmail.com>
2025-02-20 16:46:13 -0300

    Up test

---
+++ p/main copy 14.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+    "github.com/kayquelopes9/aula_03/past"
+)
+
+
+func main()  {
+	fmt.Println("Escolha qual código executar:")
+    past.MainFloat() 
+    past.MainInt()    
+}
